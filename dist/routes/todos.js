6e125d429c2081255ee4280d39a2fbf6084e73e8
5Knight2 <pratiktarale258@gmail.com>
2023-07-14 19:11:10 +0530

    Distenation&source for better file distribution

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,23 +7,26 @@ router.get('/', (req, res, next) => {
     res.json({ todos: todos });
 });
 router.post('/', (req, res, next) => {
-    const newTodo = { id: new Date().toISOString(), text: req.body.text };
+    const Body = req.body;
+    const newTodo = { id: new Date().toISOString(), text: Body.text };
     todos.push(newTodo);
     res.status(200).json({ msg: 'Added', todos: todos });
 });
 router.delete('/:id', (req, res, next) => {
-    const index = todos.findIndex((currTodo) => currTodo.id == req.params.id);
+    const Params = req.params;
+    const index = todos.findIndex((currTodo) => currTodo.id == Params.id);
     if (index >= 0) {
-        todos = todos.filter((currTodo) => { currTodo.id != req.params.id; });
+        todos = todos.filter((currTodo) => { currTodo.id != Params.id; });
         res.status(200).json({ msg: 'deleted', todos: todos });
     }
     res.status(404).json({ msg: 'Item Not Found' });
 });
 router.put('/:id', (req, res, next) => {
-    console.log(req.params.id);
-    const index = todos.findIndex((currTodo) => currTodo.id == req.params.id);
+    const Body = req.body;
+    const Params = req.params;
+    const index = todos.findIndex((currTodo) => currTodo.id == Params.id);
     if (index >= 0) {
-        todos[index].text = req.body.text;
+        todos[index].text = Body.text;
         res.status(200).json({ msg: 'updated', todos: todos });
     }
     res.status(404).json({ msg: 'Item Not Found' });
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -4,31 +4,38 @@ import{Todo} from '../models/todos'
 let todos:Todo[]=[]
 const router=Router();
 
+type reqParameter={id:string};
+type reqBody={text:string};
+
+
 router.get('/',(req,res,next)=>{
     res.json({todos:todos})
 })
 
 router.post('/',(req,res,next)=>{
-   const newTodo:Todo={id:new Date().toISOString(),text:req.body.text}
+    const Body=req.body as reqBody;
+   const newTodo:Todo={id:new Date().toISOString(),text:Body.text}
     todos.push(newTodo)
     res.status(200).json({msg:'Added',todos:todos})
 })
 router.delete('/:id',(req,res,next)=>{
-    const index=todos.findIndex((currTodo)=>currTodo.id==req.params.id)
+    const Params=req.params as reqParameter;
+    const index=todos.findIndex((currTodo)=>currTodo.id==Params.id)
     if(index>=0){
-       todos= todos.filter((currTodo)=>{currTodo.id!=req.params.id})
+       todos= todos.filter((currTodo)=>{currTodo.id!=Params.id})
        res.status(200).json({msg:'deleted',todos:todos})
     }
     res.status(404).json({msg:'Item Not Found'})
 })
 
 router.put('/:id',(req,res,next)=>{
-    console.log(req.params.id);
+    const Body=req.body as reqBody;
+    const Params=req.params as reqParameter;
     
     const index=todos.findIndex((currTodo)=>
-        currTodo.id==req.params.id)
+        currTodo.id==Params.id)
     if(index>=0){
-        todos[index].text=req.body.text;
+        todos[index].text=Body.text;
         res.status(200).json({msg:'updated',todos:todos})
     }
     res.status(404).json({msg:'Item Not Found'})
